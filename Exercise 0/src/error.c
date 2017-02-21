fa62921bb1bf4634e461fdce8bd00db73b80c5f7
Aliaksei Syrel <alex.syrel@gmail.com>
2017-02-21 15:02:31 +0100

    Solved error

--- Exercise 0/src/error.c
+++ Exercise 0/src/error.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int *f(int a) {
-	int b = 2 * a; 
-	return &b;
+void f(int a, int *b) {
+	*b = 2 * a;
 }
 
 
 int main(void) {
-	int *p4, *p8; 
-	p4 = f(4); 
-	p8 = f(8); 
-	printf("p4: %i / p8: %i\n", *p4, *p8);
+	int p4, p8;
+
+	f(4, &p4);
+	f(8, &p8);
+	printf("p4: %i / p8: %i\n", p4, p8);
 }
