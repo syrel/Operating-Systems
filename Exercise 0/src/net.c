84b68d9dfd7d7459151f843c952e513b2cf8be0e
Aliaksei Syrel <alex.syrel@gmail.com>
2017-02-22 08:38:25 +0100

    Solved net

--- Exercise 0/src/net.c
+++ Exercise 0/src/net.c
@@ -9,7 +9,10 @@ int main(void)
 	uint8_t sub[] = {255, 255, 240,   0};
 	uint8_t net[4];
 
-	/* ... */
+    size_t length = sizeof(net) / sizeof(net[0]);
+	for (size_t i = 0; i < length; i++) {
+        net[i] = add[i] & sub[i];
+	}
 
 	printf("net address: %u.%u.%u.%u\n", net[0], net[1], net[2], net[3]);
 
