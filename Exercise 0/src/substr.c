200e5fe09620f43e01b19a66fbbc6afee481c1ca
Aliaksei Syrel <alex.syrel@gmail.com>
2017-02-22 08:33:24 +0100

    Solved substring

--- Exercise 0/src/substr.c
+++ Exercise 0/src/substr.c
@@ -23,14 +23,17 @@ static void *xmalloc(size_t size)
 
 static char *substring(const char *str, off_t pos, size_t len)
 {
-	/* ... */
+	char* substr = xmalloc(len * sizeof(char));
+    // copy memory with offset from str as source to substr as destination of size len * size of char
+    memcpy(substr, &str[pos], len * sizeof(char));
+    return substr;
 }
 
 int main(int argc, char **argv)
 {
 	char *foo = "Nicht\n";
 	char *bar = substring(foo, 2, 3);
-	printf("%s", bar);
+	printf("%s\n", bar);
 	free(bar);
 	return 0;
 }
