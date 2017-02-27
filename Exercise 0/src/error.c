7782775c09d6b91e2af3b483a9084819152bd4ff
Aliaksei Syrel <alex.syrel@gmail.com>
2017-02-27 23:23:25 +0100

    Improved error.c - only change *f function body

--- Exercise 0/src/error.c
+++ Exercise 0/src/error.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void f(int a, int *b) {
+int *f(int a) {
+    int *b;
+    // instead of returning an address of a local variable located on the stack
+    // we must allocate a dynamic memory on the heap
+    b = (int*) malloc(sizeof(int));
     *b = 2 * a;
+    return b;
 }
 
-
 int main(void) {
-    int p4, p8;
+    int *p4, *p8;
 
-    f(4, &p4);
-    f(8, &p8);
-    printf("p4: %i / p8: %i\n", p4, p8);
+    p4 = f(4);
+    p8 = f(8);
+    printf("p4: %i / p8: %i\n", *p4, *p8);
 }
