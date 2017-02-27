3c4629b2689df7a2a0e24fea372dfb09fdcdc5c9
Aliaksei Syrel <alex.syrel@gmail.com>
2017-02-28 00:18:25 +0100

    Improved error.c and wrote question answers

--- Exercise 0/src/error.c
+++ Exercise 0/src/error.c
@@ -6,6 +6,10 @@ int *f(int a) {
     // instead of returning an address of a local variable located on the stack
     // we must allocate a dynamic memory on the heap
     b = (int*) malloc(sizeof(int));
+    if (!b) {
+        printf("Failed to allocate memory\n");
+        exit(1);
+    }
     *b = 2 * a;
     return b;
 }
@@ -15,5 +19,30 @@ int main(void) {
 
     p4 = f(4);
     p8 = f(8);
+
     printf("p4: %i / p8: %i\n", *p4, *p8);
+
+    free(p4);
+    free(p8);
 }
+
+
+/*
+ * IMHO better would be something like:
+ */
+/*
+
+void f(int a, int* b) {
+    *b = 2 * a;
+}
+
+int main(void) {
+    int p4, p8;
+
+    f(4, &p4);
+    f(8, &p8);
+
+    printf("p4: %i / p8: %i\n", p4, p8);
+}
+
+ */
