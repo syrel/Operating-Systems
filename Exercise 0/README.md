6049c607f0282cf9f7cf92c14be5b3aa9529ca66
Aliaksei Syrel <alex.syrel@gmail.com>
2017-02-27 11:00:44 +0100

    Added exercise 0 readme. Documented fibonacci

---
+++ Exercise 0/README.md
@@ -0,0 +1,17 @@
+# Exercise 0
+
+## Compilation
+
+	cmake . && make
+
+Binaries will be located in bin/ folder
+
+## Execute
+
+	cd bin
+	./net			# Task 2.1
+	./multiply		# Task 2.2
+	./substr		# Task 2.3
+	./fib			# Task 2.4
+	./linked_list	# Task 2.5
+	./error			# Task 2.6
--- Exercise 0/src/fib.c
+++ Exercise 0/src/fib.c
@@ -3,10 +3,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/**
+ * Return a result of fibonacci function applied on a given number
+ * @param a
+ * @return
+ */
 int fib(int a) {
+    // if argument is 0 or 1 fibonacci returns 0 or 1 respectively
     if (a == 0 || a == 1)
         return a;
 
+    // recursion approach; not the best, since we can find ourselves in a stack overflow
     return fib(a - 1) + fib(a - 2);
 }
 
