b8bbf7c93e94c9d047829c1e6b5473f3b8a19812
Aliaksei Syrel <alex.syrel@gmail.com>
2017-03-01 13:42:59 +0100

    Lowered CMake minimum version. Solved zombies

--- Exercise 1/src/zombies.c
+++ Exercise 1/src/zombies.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
-pid_t create_zombie() {
+void create_zombie() {
 	pid_t pid;
     pid = fork();
     if (pid < 0) {
@@ -15,20 +15,15 @@ pid_t create_zombie() {
 
     // in child process pid is 0
     if (pid == 0) {
-        printf("Exit child\n");
         exit(0); // instantly finish it
     }
-
-    return pid;
 }
 
 int main(void)
 {
 	unsigned int wait = 15;
-    pid_t pid;
 
-    pid = create_zombie();
-    printf("waiting on %d\n", pid);
+    create_zombie();
     sleep(wait);
 
 	return 0;
