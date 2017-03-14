cb648fbd90f3ec927ad9dd39a07f4adecb1f6fb2
Aliaksei Syrel <alex.syrel@gmail.com>
2017-03-14 08:27:12 +0100

    Documented zombies

--- Exercise 1/src/zombies.c
+++ Exercise 1/src/zombies.c
@@ -4,8 +4,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * Creates and instantly finishes a forked process producing
+ * a so called "zombie" process.
+ */
 void create_zombie() {
 	pid_t pid;
+    // create a new process and store it's pid in order to detect what which
+    // code branch to run in which process
     pid = fork();
     if (pid < 0) {
         printf("Failed to create a process\n");
@@ -20,6 +26,7 @@ void create_zombie() {
 
 int main(void)
 {
+    // for how many seconds a zombie process will live in the system
 	unsigned int wait = 15;
 
     create_zombie();
